754e84b43dc8e41f60fd7882e07b9f0acf256bea
finickyspider <finickyspider@gmail.com>
2025-07-16 22:13:54 -0700

    chore(types): update module declarations in modules.d.ts
    Added declarations for new modules 'remark-parse', 'strip-markdown', 'remark-stringify', and 'remove-markdown' to the existing list. This ensures compatibility and proper type checking when using these additional modules within the project.

--- modules.d.ts
+++ modules.d.ts
@@ -1,4 +1,8 @@
-declare module "remark-gfm";
-declare module "remark-frontmatter";
-declare module "remark-math";
-declare module "remark-footnotes";
+declare module 'remark-parse';
+declare module 'remark-gfm';
+declare module 'remark-frontmatter';
+declare module 'remark-math';
+declare module 'remark-footnotes';
+declare module 'strip-markdown';
+declare module 'remark-stringify';
+declare module 'remove-markdown';
